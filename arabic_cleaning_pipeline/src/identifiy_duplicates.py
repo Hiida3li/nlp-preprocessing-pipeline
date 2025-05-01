bdb9376e96123a8fd452c13666640a470b8d8cbc
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2025-05-01 09:44:40 +0400

    .

--- arabic_cleaning_pipeline/src/identifiy_duplicates.py
+++ arabic_cleaning_pipeline/src/identifiy_duplicates.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-df = pd.read_csv("data/raw/raw_data.csv")
+df = pd.read_csv("arabic_cleaning_pipeline/data/raw/raw_data.csv")
 
 before = len(df)
 df = df.drop_duplicates(subset='sentences', keep='first')
@@ -10,4 +10,4 @@ after = len(df)
 os.makedirs("data/deduplicated", exist_ok=True)
 df.to_csv("data/deduplicated/raw_data_deduped.csv", index=False)
 
-print(f"✅ Removed {before - after} duplicates. Remaining rows: {after}")
+print(f"Removed {before - after} duplicates. Remaining rows: {after}")
