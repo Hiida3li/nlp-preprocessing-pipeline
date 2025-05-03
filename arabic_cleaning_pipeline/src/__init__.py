2808c4c1fc6d52fe9e339d424c7c3715be922c7d
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2025-05-03 22:12:43 +0400

    modified:   arabic_cleaning_pipeline/src/__init__.py

--- arabic_cleaning_pipeline/src/__init__.py
+++ arabic_cleaning_pipeline/src/__init__.py
@@ -1,8 +1,3 @@
 from .clean_pipeline import clean_text, count_words
 from .identify_duplicates import handle_duplicates_and_missing
 
-__all__ = [
-    "clean_text",
-    "count_words",
-    "handle_duplicates_and_missing"
-]
